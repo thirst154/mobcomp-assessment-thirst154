02839fd278266f13a0282ebffa42686cf5e35330
Thomas Hirst <22504261@stu.mmu.ac.uk>
2025-02-19 16:30:25 +0000

    Attempt to add flyout page. Added test page and about  page. No content to any :)

---
+++ assignment-2425/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace assignment_2425;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ assignment-2425/TestPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace assignment_2425;
+
+public partial class TestPage : ContentPage
+{
+	public TestPage()
+	{
+		InitializeComponent();
+	}
+}
