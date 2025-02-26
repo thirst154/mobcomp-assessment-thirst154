14a849fff46c3c53d8f2e53f27f3e67c05b54d1c
Thomas Hirst <22504261@stu.mmu.ac.uk>
2025-02-26 15:41:36 +0000

    Moving towards mvvm model, WIP not working namespace error needs fixing

---
+++ assignment-2425/Model/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace assignment_2425.Model;
+
+public class Todo { 
+    //public string Id { get; set; }
+    public string Title { get; set; }
+    //public string Description { get; set; }
+    //public DateTime CreatedDate { get; set; }
+    //public DateTime UpdatedDate { get; set; }
+    public bool Status { get; set; }
+
+}
