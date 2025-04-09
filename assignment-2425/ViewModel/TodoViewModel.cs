b41b37d33216f888c8877d6829eb31840dadc444
22504261 <22504261@ASC-STU5601>
2025-04-09 10:58:19 +0100

    Users can now add new todos and remove todos from the list

--- assignment-2425/ViewModel/TodoViewModel.cs
+++ assignment-2425/ViewModel/TodoViewModel.cs
@@ -1,11 +1,30 @@
-﻿namespace assignment_2425.ViewModel;
-
-using assignment_2425.Model;
+﻿using assignment_2425.Model;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
 
-public class TodoViewModel
+namespace assignment_2425.ViewModel;
+
+
+public class TodoViewModel : INotifyPropertyChanged
 {
     public ObservableCollection<Todo> Todos { get; set; }
+
+    private string newTodoText;
+    public string NewTodoText
+    {
+        get => newTodoText;
+        set
+        {
+            if (newTodoText != value) { 
+                newTodoText = value;
+                onPropertyChanged(nameof(NewTodoText));
+            }
+        }
+    }
+
+    public ICommand AddTodoCommand { get; }
+    public ICommand DeleteTodoCommand { get; }
 
     public TodoViewModel()
     {
@@ -15,5 +34,30 @@ public class TodoViewModel
             new Todo { Text = "Walk dog" },
             new Todo { Text = "Do laundry" }
         };
+
+        AddTodoCommand = new Command(AddTodo);
+        DeleteTodoCommand = new Command<Todo>(DeleteTodo);
     }
+
+    private void AddTodo()
+    {
+        if (!string.IsNullOrWhiteSpace(NewTodoText))
+        {
+            Console.WriteLine($"Adding: {NewTodoText}");
+            Todos.Add(new Todo { Text = NewTodoText });
+            NewTodoText = string.Empty;
+
+        }
+    }
+
+    private void DeleteTodo(Todo Item)
+    {
+        if (Todos.Contains(Item)) 
+        { 
+            Todos.Remove(Item);
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    void onPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 }
